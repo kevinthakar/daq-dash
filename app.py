ea7cea88c3982fa77b84ad195e83034e7fd30fbf
Kevin Thakar <kevthakar@gmail.com>
2019-12-23 14:50:17 +0530

    Check callbacks for proper call backs

--- app.py
+++ app.py
@@ -16,115 +16,6 @@ max_length = 50
 times = deque(maxlen=max_length)
 app.config['suppress_callback_exceptions'] = True
 
-listoflist_th = []
-a_list = []
-listoflist_th.append(a_list)
-
-thermocouple_1 = deque(maxlen=max_length)
-thermocouple_2 = deque(maxlen=max_length)
-thermocouple_3 = deque(maxlen=max_length)
-thermocouple_4 = deque(maxlen=max_length)
-thermocouple_5 = deque(maxlen=max_length)
-thermocouple_6 = deque(maxlen=max_length)
-thermocouple_7 = deque(maxlen=max_length)
-thermocouple_8 = deque(maxlen=max_length)
-
-accelerometer_1 = deque(maxlen=max_length)
-accelerometer_2 = deque(maxlen=max_length)
-accelerometer_3 = deque(maxlen=max_length)
-
-
-data_thermocouple = {"Thermocouple 1": thermocouple_1,
-"Thermocouple 2": thermocouple_2,
-"Thermocouple 3": thermocouple_3,
-"Thermocouple 4":thermocouple_4,
-"Thermocouple 5":thermocouple_5,
-"Thermocouple 6":thermocouple_6,
-"Thermocouple 7":thermocouple_7,
-"Thermocouple 8":thermocouple_8}
-
-
-data_accelerometer =  {"Accelerometer 1": accelerometer_1,
-"Accelerometer 2": accelerometer_2,
-"Accelerometer 3": accelerometer_3}
-
-dict_temp = {}
-temp = ''
-vib = ''
-flow = ''
-Urange = 0
-Lrange = 0
-#dict_temp = tc1()
-
-data_analog = "Temperature"
-
-metadata_third = {"Temperature": temp,
-"Vibration": vib,
-"Flow": flow}
-
-metadata_range = {"Upper Range": Urange,
-"Lower Range": Lrange}
-
-#temp, vibration, flow = getvalue()
-tabs_corespondsensors = {'Thermocouple': ['Thermocouple 1', 'Thermocouple 2', 'Thermocouple 3',
-                                            'Thermocouple 4', 'Thermocouple 5', 'Thermocouple 6',
-                                         'Thermocouple 7', 'Thermocouple 8'],
-                        'Accelerometer': ['Accelerometer 1', 'Accelerometer 2', 'Accelerometer 3'],
-                        'Analog/Digital/IO': ['hello', 'world']
-                        }
-
-names = list(tabs_corespondsensors.keys())
-a1 = 0
-a2 = 0
-a3 = 0
-a4 = 0
-a5 = 0
-a6 = 0
-a7 = 0
-a8 = 0
-
-
-data_pins =  {"Analog 1": a1,
-"Analog 2": a2,
-"Analog 3": a3,
-"Analog 4": a4,
-"Analog 5": a5,
-"Analog 6": a6,
-"Analog 7": a7,
-"Analog 8": a8 }
-
-data_di = {"di_1": 0,
-"di_2": 0,
-"di_3": 0,
-"di_4": 0,
-"di_5": 0,
-"di_6": 0,
-"di_7": 0,
-"di_8": 0,}
-
-data_do = {"do_1": 0,
-"do_2": 0,
-"do_3": 0,
-"do_4": 0,
-"do_5": 0,
-"do_6": 0,
-"do_7": 0,
-"do_8": 0}
-
-data_sensors = []
-s = []
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-index_page = html.Div([
-    dcc.Link('Go to Page 1', href='/page-1', ),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-    html.Img(src='assets/citriot_logo (2).svg')])
-
 tab_style = {
     'borderBottom': '1px solid #d6d6d6',
 
@@ -143,6 +34,149 @@ tab_selected_style = {
     'color': 'white',
     'padding': '6px'
 }
+
+times = deque(maxlen=max_length)
+
+thermocouple_1 = deque([1,2,3,4,5,6,7,8,9,10])
+thermocouple_2 = deque([1,2,3,4,5,6,7,8,9,10])
+thermocouple_3 = deque(maxlen=50)
+thermocouple_4 = deque(maxlen=50)
+thermocouple_5 = deque(maxlen=50)
+thermocouple_6 = deque(maxlen=50)
+thermocouple_7 = deque(maxlen=50)
+thermocouple_8 = deque(maxlen=50)
+
+accelerometer_1 = deque(maxlen=50)
+accelerometer_2 = deque(maxlen=50)
+accelerometer_3 = deque(maxlen=50)
+
+
+
+data_thermocouple = {"Thermocouple 1": thermocouple_1,
+"Thermocouple 2": thermocouple_2,
+"Thermocouple 3": thermocouple_3,
+"Thermocouple 4":thermocouple_4,
+"Thermocouple 5":thermocouple_5,
+"Thermocouple 6":thermocouple_6,
+"Thermocouple 7":thermocouple_7,
+"Thermocouple 8":thermocouple_8}
+
+
+data_accelerometer =  {"Accelerometer 1": accelerometer_1,
+"Accelerometer 2": accelerometer_2,
+"Accelerometer 3": accelerometer_3}
+
+tabs_corespondsensors = {'Thermocouple': ['Thermocouple 1', 'Thermocouple 2', 'Thermocouple 3',
+                                            'Thermocouple 4', 'Thermocouple 5', 'Thermocouple 6',
+                                         'Thermocouple 7', 'Thermocouple 8'],
+                        'Accelerometer': ['Accelerometer 1', 'Accelerometer 2', 'Accelerometer 3']
+                        }
+
+names = list(tabs_corespondsensors.keys())
+
+urange1, urange2, urange3, urange4, urange5, urange6, urange7, urange8 = 0,0,0,0,0,0,0,0 #integers
+lrange1, lrange2, lrange3, lrange4, lrange5, lrange6, lrange7, lrange8 = 0,0,0,0,0,0,0,0 #integers
+do1, do2, do3, do4, do5, do6, do7, do8 = 0,0,0,0,0,0,0,0 #Boolean Values
+di1, di2, di3, di4, di5, di6, di7, di8 = 0,0,0,0,0,0,0,0 #Boolean Values
+
+data_urange = {"urange1": urange1,
+"urange2": urange2,
+"urange3": urange3,
+"urange4": urange4,
+"urange5": urange5,
+"urange6": urange6,
+"urange7": urange7,
+"urange8": urange8}
+
+data_lrange = {"lrange":lrange1,
+"lrange":lrange2,
+"lrange":lrange3,
+"lrange":lrange4,
+"lrange":lrange5,
+"lrange":lrange6,
+"lrange":lrange7,
+"lrange":lrange8,
+}
+data_di = {"di1": di1,
+"di2": di2,
+"di3": di3,
+"di4": di4,
+"di5": di5,
+"di6": di6,
+"di7": di7,
+"di8": di8}
+
+data_do = {"do1": do1,
+"do2": do2,
+"do3": do3,
+"do4": do4,
+"do5": do5,
+"do6": do6,
+"do7": do7,
+"do8": do8}
+
+
+def update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8):
+
+    times.append(time.time())
+    if len(times) == 1:
+        #starting relevant values
+
+        thermocouple_1.append(1)
+        thermocouple_2.append(20)
+        thermocouple_3.append(30)
+        thermocouple_4.append(40)
+        thermocouple_5.append(50)
+        thermocouple_6.append(60)
+        thermocouple_7.append(70)
+        thermocouple_8.append(80)
+    else:
+        for data_of_interest in [thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8]:
+            pass#data_of_interest.append(data_of_interest[-1]+data_of_interest[-1])
+
+    return times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8
+
+    times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8 = update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8)
+    print(times)
+ 
+
+def update_accelerometer_sensor(times, accelerometer_1, accelerometer_2, accelerometer_3):
+
+    times.append(time.time())
+    if len(times) == 1:
+        #starting relevant values
+        accelerometer_1.append(100)
+        accelerometer_2.append(200)
+        accelerometer_3.append(300)
+
+    else:
+        for data_of_interest in [accelerometer_1, accelerometer_2, accelerometer_3]:
+            pass#data_of_interest.append(data_of_interest[-1]+data_of_interest[-1]*random.uniform(-0.0001,0.0001))
+
+    return times, accelerometer_1, accelerometer_2, accelerometer_3
+
+
+    times, accelerometer_1, accelerometer_2, accelerometer_3, accelerometer_4, accelerometer_5, accelerometer_6, accelerometer_7, accelerometer_8 = update_thermocouple_sensor(times, accelerometer_1, accelerometer_2, accelerometer_3, accelerometer_4, accelerometer_5, accelerometer_6, accelerometer_7, accelerometer_8)
+
+
+def analog_digital_func():
+
+    return 0
+
+app.layout = html.Div([
+        dcc.Location(id='url', refresh=False),
+        html.Div(id='page-content')
+])
+
+
+index_page = html.Div([
+
+    html.Div([html.H3('Welcome to C-DAQ!')], style={'text-align':'center'}),
+    html.Div([html.Img(src='assets/image2vector.svg')], style={'text-align':'center'}),
+    html.Div([dcc.Link('Go to Digital Sensors', href='/page-1')], style={'text-align':'center'}),
+    html.Br(),
+    html.Div(dcc.Link('Go to Analog/Digital Inputs', href='/page-2'), style={'text-align':'center'}),
+    ])
 
 page_1_layout = html.Div([
 
@@ -170,26 +204,21 @@ page_1_layout = html.Div([
         
         ]),
                     html.Div([
-                        dcc.Tabs(id="input-source-1", value=names[0], style=tab_style, children=[
+                        dcc.Tabs(id="tabselect", value=names[0], style=tab_style, children=[
                             dcc.Tab(label='Thermocouple', value=names[0], style=tab_style, selected_style=tab_selected_style),
                             dcc.Tab(label='Accelerometer', value=names[1], style=tab_style, selected_style=tab_selected_style),
                                 ],),
                         ]),
 
-                    html.Div(id= 'output-source-11', children=
-                            dcc.Dropdown(id='output-source-1',
-                                #value=['Accelerometer 1'],
+                    html.Div([dcc.Dropdown(id='dropdownlist',
+                                value=['Thermocouple 1'],
                                 multi=True),
-                            ),
-
-                    html.Div(id='output-source-22',children=[
-                            dcc.Graph(id='output-source-2'),
-                            dcc.Interval(
-                                    id='graph-update',
-                                    interval=1000)],
-                            className="container",style={'width':'100%','max-width':50000})
-])
-
+                    ]),
+                    html.Div(children=html.Div(id='graphs'), className='row'),
+                    dcc.Interval(
+                    id='graph-update',
+                    interval=100),
+                ], className="container",style={'width':'98%','margin-left':10,'margin-right':10,'max-width':50000})
 
 page_2_layout = html.Div([
 
@@ -487,53 +516,9 @@ def display_page(pathname):
     else:
         return index_page
         
-def update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8):
 
-    times.append(time.time())
-    if len(times) == 1:
-        #starting relevant values
-        thermocouple_1.append(random.randrange(180,230))
-        thermocouple_2.append(random.randrange(95,115))
-        thermocouple_3.append(random.randrange(170,220))
-        thermocouple_4.append(random.randrange(1000,9500))
-        thermocouple_5.append(random.randrange(30,140))
-        thermocouple_6.append(random.randrange(10,90))
-    else:
-        for data_of_interest in [thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6]:
-            data_of_interest.append(data_of_interest[-1]+data_of_interest[-1]*random.uniform(-0.0001,0.0001))
-
-    return times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8
-
-    times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8 = update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8)
- 
-
-def update_accelerometer_sensor(times, accelerometer_1, accelerometer_2, accelerometer_3):
-
-    times.append(time.time())
-    if len(times) == 1:
-        #starting relevant values
-        accelerometer_1.append(random.randrange(180,230))
-        accelerometer_2.append(random.randrange(95,115))
-        accelerometer_3.append(random.randrange(170,220))
-
-    else:
-        for data_of_interest in [accelerometer_1, accelerometer_2, accelerometer_3]:
-            data_of_interest.append(data_of_interest[-1]+data_of_interest[-1]*random.uniform(-0.0001,0.0001))
-
-    return times, accelerometer_1, accelerometer_2, accelerometer_3
-
-
-    times, accelerometer_1, accelerometer_2, accelerometer_3, accelerometer_4, accelerometer_5, accelerometer_6, accelerometer_7, accelerometer_8 = update_thermocouple_sensor(times, accelerometer_1, accelerometer_2, accelerometer_3, accelerometer_4, accelerometer_5, accelerometer_6, accelerometer_7, accelerometer_8)
-
-
-def analog_digital_func():
-    return 0
-
-# times, acc1, acc2... = update 
-#times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6 = update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6)
-
-@app.callback(dash.dependencies.Output('output-source-1', 'options'),
-              [dash.dependencies.Input('input-source-1', 'value')])
+@app.callback(dash.dependencies.Output('dropdownlist', 'options'),
+              [dash.dependencies.Input('tabselect', 'value')])
 
 def render_content(value):
 
@@ -550,29 +535,33 @@ def render_content(value):
 
             return [{'label': i, 'value': i} for i in tabs_corespondsensors[value]]
 
-@app.callback(dash.dependencies.Output('output-source-2', 'children'),
-              [dash.dependencies.Input('output-source-1', 'value')])
 
-def render_content(value):
+@app.callback(dash.dependencies.Output('graphs', 'children'),
+              [dash.dependencies.Input('dropdownlist', 'value'), dash.dependencies.Input('graph-update', 'n_intervals')])
 
-    th_no = value
+def update_graph(data_names, n):
+
     graphs = []
+    update_thermocouple_sensor(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8)
+    update_accelerometer_sensor(times, accelerometer_1, accelerometer_2, accelerometer_3)
 
-    data = go.Scatter(
+    for data_name in data_names:
+
+        data = go.Scatter(
             x=list(times),
-            y=list(data_thermocouple[value]),
+            y=list(data_thermocouple[data_name]),
             name='Scatter',
             fill="tozeroy",
             fillcolor="#6897bb"
             )
 
-    graphs.append(html.Div(dcc.Graph(
-            id=value,
+        graphs.append(html.Div(dcc.Graph(
+            id=data_name,
             animate=True,
             figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(times),max(times)]),
-                                                        yaxis=dict(range=[min(data_thermocouple.get(value)),max(data_thermocouple.get(value))]),
+                                                        yaxis=dict(range=[min(data_thermocouple[data_name]),max(data_thermocouple[data_name])]),
                                                         margin={'l':50,'r':1,'t':45,'b':1},
-                                                        title='{}'.format(value))}
+                                                        title='{}'.format(data_name))}
             )))
 
     return graphs
