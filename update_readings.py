c5b217f3a8a08edb147933ee18d2ed9226cc38c9
Citriot <citriotdata@gmail.com>
2020-01-03 06:41:27 +0000

    10 readings per refresh

--- update_readings.py
+++ update_readings.py
@@ -16,20 +16,23 @@ from components_layout import *
 
 def update_thermocouple_values(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8):
 
-    times.append(time.time())
-    l = temp_data()
-    print(l)
+        i = 0
+        while i<=10:
+            times.append(time.time())
+            l = temp_data()
+            print(l)
     #if len(times) == 1:
-    while True:#starting relevant values
-        thermocouple_1.append(l[0])
-        thermocouple_2.append(l[1])
-        thermocouple_3.append(l[2])
-        thermocouple_4.append(l[3])
-        thermocouple_5.append(l[4])
-        thermocouple_6.append(l[5])
-        thermocouple_7.append(l[6])
-        thermocouple_8.append(l[7])
-
+    #starting relevant values
+            thermocouple_1.append(l[0])
+            thermocouple_2.append(l[1])
+            thermocouple_3.append(l[2])
+            thermocouple_4.append(l[3])
+            thermocouple_5.append(l[4])
+            thermocouple_6.append(l[5])
+            thermocouple_7.append(l[6])
+            thermocouple_8.append(l[7])
+            i = i+1
+        
         return times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8
 
 times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8 = update_thermocouple_values(times, thermocouple_1, thermocouple_2, thermocouple_3, thermocouple_4, thermocouple_5, thermocouple_6, thermocouple_7, thermocouple_8)
